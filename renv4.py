0745b212c6547195dbd9962dcbda39ad3c5f3d8c
Jefferson Pascual <103686055+asdJPasc@users.noreply.github.com>
2023-05-24 17:21:40 +0800

    Update renv4.py
    Exclude "Do not delete" Folder from renaming the files

--- renv4.py
+++ renv4.py
@@ -8,7 +8,7 @@ def rename_files(folder_path, day):
     files = sorted(files, key=lambda f: os.stat(os.path.join(folder_path, f)).st_mtime)
 
     for subfolder in os.listdir(folder_path):
-        if not os.path.isdir(os.path.join(folder_path, subfolder)):
+        if not os.path.isdir(os.path.join(folder_path, subfolder)) or subfolder == "DO NOT DELETE":
             continue
 
         subfolder_path = os.path.join(folder_path, subfolder)
@@ -18,15 +18,15 @@ def rename_files(folder_path, day):
         sub_files = sorted(sub_files, key=lambda f: os.stat(os.path.join(subfolder_path, f)).st_mtime)
 
         for i, filename in enumerate(sub_files):
-            if os.path.isdir(os.path.join(subfolder_path, filename)):
+            if os.path.isdir(os.path.join(subfolder_path, filename)): 
                 continue
 
             file_name, file_extension = os.path.splitext(filename)
 
             new_file_name = f"{day}{file_name[5:]}{file_extension}"
-
+            
             os.rename(os.path.join(subfolder_path, filename), os.path.join(subfolder_path, new_file_name))
 
-day = input("Enter the day (in MM-DD format) for the file name: ")
+day = input("Enter the day (in MM-DD format) for the file name: ") #04-20
 
 rename_files(os.getcwd(), day)
